15fb3470c3adccb420401d67bb0546b3db330134
lastpatrol <lastpatrol09@gmail.com>
2019-03-20 01:00:44 +0300

    fixed minor linter errors

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import { cons, car, cdr, toString } from 'hexlet-pairs';
-import { welcome, greetAndGetName,  } from '..';
+import { welcome, greetAndGetName } from '..';
 
 welcome();
 greetAndGetName();
