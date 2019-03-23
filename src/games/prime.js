ef82ca2a994c1d6be4017b25b88932496fb16601
lastpatrol <lastpatrol09@gmail.com>
2019-03-23 16:59:06 +0300

    added external module for prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,30 +1,11 @@
 import play from '..';
 import { makeStep, getRandom } from '../utils';
+import isPrime from 'prime-number';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (inputNum) => {
-  if (inputNum < 2) {
-    return false;
-  }
-
-  const iter = (num, counter) => {
-    if (counter > num / 2) {
-      return true;
-    }
-
-    if (num % counter === 0) {
-      return false;
-    }
-
-    return iter(num, counter + 1);
-  };
-
-  return iter(inputNum, 2);
-};
-
 const makeGameData = () => {
-  const question = getRandom(1, 99);
+  const question = getRandom(1, 200);
   const rightAnswer = isPrime(question) ? 'yes' : 'no';
   return makeStep(question, rightAnswer);
 };
